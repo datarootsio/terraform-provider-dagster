dead93cc877b6eee91b00d3965fbd2f257190243
Nico Gelders <nicolas.gelders@telenetgroup.be>
2024-04-09 22:39:53 +0200

    docs

---
+++ examples/resources/dagster_team_deployment_grant/resource.tf
@@ -0,0 +1,12 @@
+data "dagster_current_deployment" "current" {}
+
+resource "dagster_team" "example" {
+  name = "example_team"
+}
+
+resource "dagster_team_deployment_grant" "example" {
+  deployment_id = data.dagster_current_deployment.current.id
+  team_id       = dagster_team.example.id
+
+  grant = "viewer"
+}
