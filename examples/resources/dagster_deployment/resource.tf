b416aec2403f73b7ec77ac33ad0e5836bee1f547
Quinten Bruynseraede <quinten.bruynseraede@hotmail.com>
2024-04-23 16:37:13 +0200

    Better docs

--- examples/resources/dagster_deployment/resource.tf
+++ examples/resources/dagster_deployment/resource.tf
@@ -1,30 +1,26 @@
 resource "dagster_deployment" "this" {
-  name = "deploy"
-  settings_document = trimspace(<<TEXT
-{
-  "run_queue": {
-    "max_concurrent_runs": 10,
-    "tag_concurrency_limits": []
-  },
-  "run_monitoring": {
-    "start_timeout_seconds": 1200,
-    "cancel_timeout_seconds": 1200,
-    "free_slots_after_run_end_seconds": 300
-  },
-  "run_retries": {
-    "max_retries": 0,
-    "retry_on_asset_or_op_failure": true
-  },
-  "sso_default_role": "VIEWER",
-  "non_isolated_runs": {
-    "max_concurrent_non_isolated_runs": 1
-  },
-  "auto_materialize": {
-    "run_tags": {},
-    "respect_materialization_data_versions": false,
-    "use_sensors": false
-  }
+  name              = "test-deploy"
+  settings_document = data.dagster_deployment_settings_document.this.json
 }
-TEXT
-  )
+
+data "dagster_deployment_settings_document" "this" {
+  yaml_body = <<YAML
+run_queue:
+  max_concurrent_runs: 30
+  tag_concurrency_limits: []
+run_monitoring:
+  start_timeout_seconds: 1200
+  cancel_timeout_seconds: 1400
+  free_slots_after_run_end_seconds: 300
+run_retries:
+  max_retries: 0
+  retry_on_asset_or_op_failure: true
+sso_default_role: VIEWER
+non_isolated_runs:
+  max_concurrent_non_isolated_runs: 1
+auto_materialize:
+  run_tags: {}
+  respect_materialization_data_versions: false
+  use_sensors: false
+YAML
 }
