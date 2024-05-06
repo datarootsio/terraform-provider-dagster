91bf5dd29b44fc5613ca1df3d8520c75e68660c2
Nico Gelders <nicolas.gelders@telenetgroup.be>
2024-05-06 08:14:11 +0200

    docs adjust

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,10 +1,11 @@
-# With Dagster Cloud CLI env vars
+# With Dagster Cloud CLI Env vars
 # - DAGSTER_CLOUD_ORGANIZATION
 # - DAGSTER_CLOUD_DEPLOYMENT
 # - DAGSTER_CLOUD_API_TOKEN
 provider "dagster" {}
 
 # Explicitly
+# Note: Explicit configuration in the provider block takes precedences over the Env vars from the example above.
 provider "dagster" {
   organization = var.organization
   deployment   = var.deployment
