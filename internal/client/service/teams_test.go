33380bc8c6a21233ea67e9b0eb606f4630f4fc50
Quinten <quinten@dataroots.io>
2024-05-08 20:34:53 +0200

    Add teams_tests.go

--- internal/client/service/teams_test.go
+++ internal/client/service/teams_test.go
@@ -5,20 +5,16 @@ import (
 	"os"
 	"testing"
 
-	"github.com/datarootsio/terraform-provider-dagster/internal/client"
+	"github.com/datarootsio/terraform-provider-dagster/internal/client/schema"
 	"github.com/datarootsio/terraform-provider-dagster/internal/client/types"
+	"github.com/datarootsio/terraform-provider-dagster/internal/testutils"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestTeamsService_BasicCRUD(t *testing.T) {
-	organization := os.Getenv("TF_VAR_testing_dagster_organization")
-	deployment := os.Getenv("TF_VAR_testing_dagster_deployment")
-	apiToken := os.Getenv("TF_VAR_testing_dagster_api_token")
+	client := testutils.GetDagsterClientFromEnvVars()
 
 	ctx := context.Background()
-
-	client, err := client.NewDagsterClient(organization, deployment, apiToken)
-	assert.NoError(t, err)
 
 	teamsClient := client.TeamsClient
 
@@ -27,7 +23,7 @@ func TestTeamsService_BasicCRUD(t *testing.T) {
 	teamNameRenamed := "testing/my_team_renamed"
 
 	// Ensure no teams with the test names exist
-	_, err = teamsClient.GetTeamByName(ctx, teamName)
+	_, err := teamsClient.GetTeamByName(ctx, teamName)
 	assert.ErrorAs(t, err, &errNotFound)
 
 	_, err = teamsClient.GetTeamByName(ctx, teamNameRenamed)
@@ -61,5 +57,90 @@ func TestTeamsService_BasicCRUD(t *testing.T) {
 	assert.ErrorAs(t, err, &errNotFound)
 
 	_, err = teamsClient.GetTeamByName(ctx, teamNameRenamed)
+	assert.ErrorAs(t, err, &errNotFound)
+}
+
+func TestTeamsDeploymentGrants(t *testing.T) {
+	client := testutils.GetDagsterClientFromEnvVars()
+
+	ctx := context.Background()
+	deploymentClient := client.DeploymentClient
+	teamsClient := client.TeamsClient
+	codelocationClient := client.CodeLocationsClient
+
+	deploymentName := os.Getenv("TF_VAR_testing_dagster_deployment")
+	if deploymentName == "" {
+		t.Errorf("Deployment with name %s not found!", deploymentName)
+	}
+	deployment, err := deploymentClient.GetDeploymentByName(ctx, deploymentName)
+	if err != nil {
+		t.Errorf("Error getting deployment with name %s", deploymentName)
+	}
+
+	teamName := "test_team3"
+	codeLocation := types.CodeLocation{
+		Name:  "testing_codelocation",
+		Image: "test123_2",
+		CodeSource: types.CodeLocationCodeSource{
+			PythonFile: "test.py-2",
+		},
+	}
+	var errNotFound *types.ErrNotFound
+
+	// Ensure no codelocation or team with the test name exist
+	_, err = teamsClient.GetTeamByName(ctx, teamName)
+	assert.ErrorAs(t, err, &errNotFound)
+	_, err = codelocationClient.GetCodeLocationByName(ctx, codeLocation.Name)
+	assert.ErrorAs(t, err, &errNotFound)
+
+	// Create team, code location and deployment
+	team, err := teamsClient.CreateTeam(ctx, teamName)
+	assert.NoError(t, err)
+	err = codelocationClient.AddCodeLocation(ctx, codeLocation)
+	assert.Nil(t, err)
+	t.Cleanup(func() {
+		teamsClient.DeleteTeam(ctx, team.Id)
+		codelocationClient.DeleteCodeLocation(ctx, codeLocation.Name)
+	})
+
+	// Check current deployment grant doesn't exist
+	_, err = teamsClient.GetTeamDeploymentGrantByTeamAndDeploymentId(ctx, team.Id, deployment.DeploymentId)
+	assert.ErrorAs(t, err, &errNotFound)
+
+	// Set grant to ADMIN and ADMIN on the codelocation and check result
+	grant, err := teamsClient.CreateOrUpdateTeamDeploymentGrant(
+		ctx,
+		team.Id,
+		deployment.DeploymentId,
+		schema.PermissionGrantViewer,
+		[]schema.LocationScopedGrant{
+			{LocationName: codeLocation.Name, Grant: schema.PermissionGrantLauncher},
+		},
+	)
+	assert.NoError(t, err)
+	assert.Equal(t, grant.DeploymentId, deployment.DeploymentId)
+	assert.Equal(t, grant.Grant, schema.PermissionGrantViewer)
+
+	for _, locationGrant := range grant.LocationGrants {
+		if locationGrant.LocationName == codeLocation.Name {
+			assert.Equal(t, schema.PermissionGrantLauncher, locationGrant.Grant)
+		}
+	}
+
+	// Read the grant again and ensure the results are consistent
+	grantRead, err := teamsClient.GetTeamDeploymentGrantByTeamAndDeploymentId(ctx, team.Id, deployment.DeploymentId)
+	assert.NoError(t, err)
+	assert.Equal(t, grantRead.DeploymentId, deployment.DeploymentId)
+	assert.Equal(t, grantRead.Grant, schema.PermissionGrantViewer)
+	for _, locationGrant := range grantRead.LocationGrants {
+		if locationGrant.LocationName == codeLocation.Name {
+			assert.Equal(t, schema.PermissionGrantLauncher, locationGrant.Grant)
+		}
+	}
+
+	// Remove the grant and check the result
+	err = teamsClient.RemoveTeamDeploymentGrant(ctx, team.Id, deployment.DeploymentId)
+	assert.NoError(t, err)
+	_, err = teamsClient.GetTeamDeploymentGrantByTeamAndDeploymentId(ctx, team.Id, deployment.DeploymentId)
 	assert.ErrorAs(t, err, &errNotFound)
 }
