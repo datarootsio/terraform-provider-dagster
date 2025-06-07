109db189fadebbde9379bc807c9e635384be603d
Quinten Bruynseraede <quinten.bruynseraede@hotmail.com>
2025-06-07 14:47:18 +0200

    Add e2e test

---
+++ examples/e2e/instance/main.tf
@@ -0,0 +1,37 @@
+variable "organization" {
+  type = string
+}
+
+variable "deployment" {
+  type = string
+}
+
+variable "api_token" {
+  type = string
+}
+
+terraform {
+  required_version = ">= 1.3.5"
+
+  required_providers {
+    dagster = {
+      source = "datarootsio/dagster"
+    }
+  }
+
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
+
+provider "dagster" {
+  organization = var.organization
+  deployment   = var.deployment
+  api_token    = var.api_token
+}
+
+data "dagster_version" "this" {}
+
+output "version" {
+  value = data.dagster_version.this.version
+}
